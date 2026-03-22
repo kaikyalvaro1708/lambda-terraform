e7ffc85be17efe9defae263639bd36ee8a7408ec
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2026-03-22 20:50:43 -0300

    lambda name adjusted

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "lambda_name" {
   description = "lambda_test_terraform"
   type        = string
-  default     = "lambda_collecto"
+  default     = "lambda_collector"
 }
 
 variable "lambda_runtime" {
