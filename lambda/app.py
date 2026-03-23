9c805c771527254d012b30da290b7c9cef06d114
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2026-03-22 21:00:47 -0300

    lambda adjusted

--- lambda/app.py
+++ lambda/app.py
@@ -1,12 +1,4 @@
 import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
-import json
 import urllib.request
 import urllib.parse
 import boto3
